54575369962a7936b21d26bf81f154feeebf5435
Caio dos Santos Rodrigues <caiodossantosrodrigues@yahoo.com.br>
2022-11-22 16:18:48 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,37 +4,37 @@ def text_decorator(text: str, color: str = 'white', decorator: str = '=',) -> No
 
     if color == 'white':
         print(decorator * (len(text) + 2))
-        print(f' {text}')
+        print('', coloring_text(text, 'white'))
         print(decorator * (len(text) + 2))
     
     elif color == 'red':
         print(decorator * (len(text) + 2))
-        print(f' \033[1;31m{text}\033[m')
+        print('', coloring_text(text, 'red'))
         print(decorator * (len(text) + 2))
     
     elif color == 'green':
         print(decorator * (len(text) + 2))
-        print(f' \033[1;32m{text}\033[m')
+        print('', coloring_text(text, 'green'))
         print(decorator * (len(text) + 2))
     
     elif color == 'yellow':
         print(decorator * (len(text) + 2))
-        print(f' \033[1;33m{text}\033[m')
+        print('', coloring_text(text, 'yellow'))
         print(decorator * (len(text) + 2))
 
     elif color == 'purple':
         print(decorator * (len(text) + 2))
-        print(f' \033[1;34m{text}\033[m')
+        print('', coloring_text(text, 'purple'))
         print(decorator * (len(text) + 2))
 
     elif color == 'pink':
         print(decorator * (len(text) + 2))
-        print(f' \033[1;35m{text}\033[m')
+        print('', coloring_text(text, 'pink'))
         print(decorator * (len(text) + 2))
 
     elif color == 'cian':
         print(decorator * (len(text) + 2))
-        print(f' \033[1;36m{text}\033[m')
+        print('', coloring_text(text, 'cian'))
         print(decorator * (len(text) + 2))
     
     print()
@@ -50,7 +50,6 @@ def main_decorator():
 def clean():
 
     os.system('cls' if os.name == 'nt' else 'clear')
-
 
 def coloring_text(text, color):
 
@@ -73,6 +72,10 @@ def coloring_text(text, color):
     if color == 'purple':
 
         return(f'\033[1;34m{text}\033[m')
+
+    if color == 'pink':
+
+        return(f'\033[1;35m{text}\033[m')
 
     if color == 'cian':
 
