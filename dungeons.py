7ed4102f3737a274dfced3b3d0e1b205e0b18e63
Caio dos Santos Rodrigues <caiodossantosrodrigues@yahoo.com.br>
2022-11-25 17:28:26 -0300

    Update dungeons.py

--- dungeons.py
+++ dungeons.py
@@ -1,3 +1,4 @@
+from floors import d1_floor1
 from utils import animated_text, clean, text_decorator
 
 
@@ -5,24 +6,25 @@ def dungeon_decorator(dungeon):
 
     clean()
 
-    text_decorator(dungeon, color='cian')
+    text_decorator(f'         {dungeon}         ', color='cian')
 
 
 def dungeon1(player):
 
     # INTRODUÇÃO A DUNGEON
 
-    dungeon_decorator('         Dungeon 1         ')
+    dungeon_decorator('Dungeon 1')
 
     animated_text(f'Voz Desconhecida: Olá {player.name}, sou Isabely e serei sua guia nesta jornada.\nQuando você finaliza-la poderei te explicar sobre como usar as habilidades que\nvocê irá despertar no caminho... se você conseguir finalizar a dungeon, é claro.\nDesejo-lhe boa sorte em sua primeira dungeon.')
 
     input('\n\nPressione enter para continuar...')
 
-    dungeon_decorator('         Dungeon 1         ')
+    dungeon_decorator('Dungeon 1')
 
-    animated_text(f'\033[1;35mIsabely\033[m: A primeira dungeon foi dominada pelos goblins, cada Dungeon tem\n10 andares, você tera que limpar cada um deles para poder finaliza-la.')
+    animated_text(f'\033[1;35mIsabely\033[m: A primeira dungeon foi dominada pelos slimes, cada Dungeon tem\n10 andares, você tera que limpar cada um deles para poder finaliza-la.')
 
     input('\n\nPressione enter para continuar...')
     
+    d1_floor1(player)
     
 
